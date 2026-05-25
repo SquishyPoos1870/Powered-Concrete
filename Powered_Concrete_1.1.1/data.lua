b184b7976b99152146558863e2d53be63df880ec
Squishy1870 <camgrieve1870@gmail.com>
2026-05-25 12:15:31 +1000

    Delete Powered_Concrete_1.1.1 directory

--- Powered_Concrete_1.1.1/data.lua
+++
@@ -1,6 +0,0 @@
-require("prototypes.entity")
-require("prototypes.item-group")
-require("prototypes.tile")
-require("prototypes.item")
-require("prototypes.recipe")
-require("prototypes.technology")
